a81c0802f822bde289959a8cf637aa003e12e201
oletobmoe <61150284+oletobmoe@users.noreply.github.com>
2020-03-02 16:28:29 +0100

    fortsatt ingeng aning

---
+++ AVR/Solder paste printer/Header.h
@@ -0,0 +1,26 @@
+/*
+ * Header.h
+ *
+ * Created: 19-Feb-20 15:32:47
+ *  Author: mikda
+ */ 
+
+
+#ifndef HEADER_H_
+#define HEADER_H_
+
+#include <avr/io.h>
+#include <stdlib.h>
+<<<<<<< HEAD
+#include <avr/interrupt.h>
+=======
+#include <stdbool.h>
+#include <math.h>
+>>>>>>> f02e13aa166c5b876309867dada4ea082c3fe0f8
+#include "Utility.h"
+#include "Config.h"
+#include "GParse.h"
+#include "stepper.h"
+#include "USART.h"
+
+#endif /* HEADER_H_ */
